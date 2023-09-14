4981a457d4ebcc18d807bda6f11a22b4c80a02ca
mochioSP <keeee50114@gmail.com>
2023-09-14 23:41:49 +0900

    first commit サイドバーの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { SideBar } from "./01_Sidebar/Sidebar";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <SideBar />
     </div>
   );
 }
